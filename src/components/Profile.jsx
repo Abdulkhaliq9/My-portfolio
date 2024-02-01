b7636243301342a257f43f2bdea2a0a93e5d0942
Abdul Khaliq <108611214+Abdulkhaliq9@users.noreply.github.com>
2024-02-01 13:09:28 +0500

    done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../css/theme.css";
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
-import CV from "../assets/cv.pdf"
+import CV from "../assets/images/Profile.png"
 
 export default function Profile() {
 
